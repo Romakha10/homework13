f38811fd34a55f75ac45b2fd1313f0748e920559
Romakha10 <makharadzer1@gmail.com>
2022-10-24 18:01:42 -0700

    final

---
+++ main.js
@@ -0,0 +1,19 @@
+let clubYears = 1899;
+let club = "Barcelona";
+let fansName = "cule";
+let conssesion = true;
+let firstChamp = 1929;
+
+const myName = "roma";
+const myHobby = "play football";
+const yearOfBirth = 1996;
+const currentYear = 2022;
+
+const myAge = currentYear - yearOfBirth;
+
+const myName2 = "My name";
+const is = "is";
+
+const name = myName2 + is;
+
+console.log(myAge, name);
